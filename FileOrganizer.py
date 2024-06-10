880440fe1911c8a0880f08ffc90adfc1ca526de8
CyberRift <107774155+Dreadwolf26@users.noreply.github.com>
2024-06-09 21:02:16 -0400

    Update FileOrganizer.py
    emulation. failing dueto path not being found will revised at a later date.

--- FileOrganizer.py
+++ FileOrganizer.py
@@ -10,7 +10,7 @@ source_dir = "C:\\Users\\chris\\Downloads"
 target_dir = "C:\\Users\\chris\\Documents"
 
 #Create destination folders if they don't exist
-categories = ['Images', 'Documents', 'Music', 'Videos', 'Emulators']
+categories = ['Images', 'Documents', 'Music', 'Videos', 'Games', 'PSP','Emulators']
 for category in categories:
     category_path = os.path.join(target_dir, category)
     os.makedirs(category_path, exist_ok=True)
@@ -29,7 +29,31 @@ def sort_files_by_type(source_dir, target_dir):
                 shutil.move(file_path, os.path.join(target_dir, 'Music', filename))
             elif file_extension in ('mp4', 'avi', 'mkv','mov','wmv', 'flv','3gp','mpeg', 'webm', 'vob', 'rm', 'swf', 'ts'):
                 shutil.move(file_path, os.path.join(target_dir, 'Videos', filename))
-            elif file_extension in ('z64', 'bin', 'ciso','md','iso', 'smc'):
-                shutil.move(file_path, os.path.join(target_dir, 'Emulators', filename))
+
+
+
+#Emulation defined not working system cannot find path specified. May have to set a for oop again with the given categoies like fm above. 
+                
+source_dir_e = "C:\\Users\\chris\\Downloads"
+#Target drectory to wear you file will be saved 
+target_dir_e = "C:\\Users\\chris\\Documents\\Emulators\\Games"
+
+def sort_files_by_emulation(source_dir_e, target_dir_e):
+    for filename in os.listdir(source_dir):
+        file_path = os.path.join(source_dir, filename)
+        if os.path.isfile(file_path):
+            file_extension = filename.split('.')[-1].lower()
+            if file_extension in ('z64'):
+                shutil.move(file_path, os.path.join(target_dir, 'N64', filename))
+            elif file_extension in ('bin'):
+                shutil.move(file_path, os.path.join(target_dir, 'DreamCast', filename))
+            elif file_extension in ('ciso'):
+                shutil.move(file_path, os.path.join(target_dir, 'GameCube', filename))
+            elif file_extension in ('md'):
+                shutil.move(file_path, os.path.join(target_dir, 'Genesis', filename))
+            elif file_extension in ('iso'):
+                shutil.move(file_path, os.path.join(target_dir, 'PSP', filename))
+
 
 sort_files_by_type(source_dir, target_dir)
+sort_files_by_emulation(source_dir_e, target_dir_e)
